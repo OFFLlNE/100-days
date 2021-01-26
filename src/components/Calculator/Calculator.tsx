ea687aec021c0dc1efa9d07a547ccdbf77ad4943
OFFLlNE <lauri.valja@transferwise.com>
2021-01-26 10:20:00 +0200

    Trying to add some math logic

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -13,9 +13,46 @@ import "./Calculator.scss";
 
 // eslint-disable-next-line
 const CALCULATOR_BUTTONS = ["1","2","3","*","4","5","6","/","7","8","9","+","AC","0","=", "-"];
+const NUMBER_STRINGS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 const Calculator = (): JSX.Element => {
   const [input, setInput] = useState("");
+  const [numberMemory, setNumberMemory] = useState(null);
+  const [currentOperator, setCurrentOperator] = useState(null);
+
+  const handleActionOnInput = ({variable}) => {
+    if (NUMBER_STRINGS.includes(variable)) return setInput(input + variable);
+    console.log("Input here? ", input);
+
+    setNumberMemory(parseFloat(input));
+    setInput("");
+    const symbol = variable;
+    // For some reason log seems to happen too fast that the state is not updated yet
+    console.log("Symbol and saved input: ", symbol, numberMemory);
+
+    // if (prevNumber) return operatorFunction[symbol](prevNumber, newNumber);
+    // if (symbol === "=") return operatorFunction[currentOperator](prevNumber, newNumber);
+    // switch (symbol) {
+    //   case "/": {
+    //     return prevNumber / newNumber;
+    //   }
+    //   case "*": {
+    //     return prevNumber * newNumber;
+    //   }
+    //   case "+": {
+    //     return prevNumber + newNumber;
+    //   }
+    //   case "-": {
+    //     return prevNumber - newNumber;
+    //   }
+    //   case "AC": {
+    //     return 0;
+    //   }
+    //   default: {
+    //     return 1;
+    //   }
+    // }
+  };
 
   return (
     <>
@@ -24,7 +61,11 @@ const Calculator = (): JSX.Element => {
           <input value={input} />
         </div>
         {CALCULATOR_BUTTONS.map((buttonContent) => (
-          <CalculatorButton key={buttonContent} content={buttonContent} onButtonClick={setInput} />
+          <CalculatorButton
+            key={buttonContent}
+            content={buttonContent}
+            onButtonClick={(value) => handleActionOnInput({variable: value})}
+          />
         ))}
       </div>
     </>
@@ -37,6 +78,26 @@ const CalculatorButton = (prop: {content: string; onButtonClick: any}): JSX.Elem
       {prop.content}
     </button>
   );
+};
+
+function checkIfNumber(toBeChecked) {
+  return toBeChecked === typeof "number";
+}
+
+// Click on symbol
+// if there is numberMemory
+//// return numbermemory (symbol) newNumber
+// if symbol === "="
+//// return numberMemory currentOperator newNumber
+// return {newNumber, symbolAction}
+// Clear the current input
+
+const operatorFunction = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "*": (x, y) => x * y,
+  "/": (x, y) => x / y,
+  "": (x, y) => y
 };
 
 export default Calculator;
