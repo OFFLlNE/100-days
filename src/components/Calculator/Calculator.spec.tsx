3b00cc20d1e4fc909a62a94bf521fba571894677
OFFLlNE <lauri.valja@transferwise.com>
2021-02-04 18:17:55 +0200

    Add basic math operation tests

--- src/components/Calculator/Calculator.spec.tsx
+++ src/components/Calculator/Calculator.spec.tsx
@@ -3,12 +3,12 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Calculator from './Calculator';
 
 describe('Calculator', () => {
-  it('calculates the sum', () => {
+  it('calculates the addition', () => {
     render(<Calculator />);
 
+    const numberFive = screen.getByText('5');
     const addOperator = screen.getByText('+');
     const numberSeven = screen.getByText('7');
-    const numberFive = screen.getByText('5');
     const equals = screen.getByText('=');
 
     fireEvent.click(numberFive);
@@ -16,8 +16,56 @@ describe('Calculator', () => {
     fireEvent.click(numberSeven);
     fireEvent.click(equals);
 
-    const sumOfFiveAndSeven = screen.getByDisplayValue('12');
+    expect(screen.getByDisplayValue('12')).toBeInTheDocument();
+  });
 
-    expect(sumOfFiveAndSeven).toBeInTheDocument();
+  it('calculates the multiplication', () => {
+    render(<Calculator />);
+
+    const numberFive = screen.getByText('5');
+    const multiplyOperator = screen.getByText('*');
+    const numberSeven = screen.getByText('7');
+    const equals = screen.getByText('=');
+
+    fireEvent.click(numberFive);
+    fireEvent.click(multiplyOperator);
+    fireEvent.click(numberSeven);
+    fireEvent.click(equals);
+
+    expect(screen.getByDisplayValue('35')).toBeInTheDocument();
+  });
+
+  it('calculates the subtraction', () => {
+    render(<Calculator />);
+
+    const numberFive = screen.getByText('5');
+    const subtractOperator = screen.getByText('-');
+    const numberSeven = screen.getByText('7');
+    const equals = screen.getByText('=');
+
+    fireEvent.click(numberFive);
+    fireEvent.click(subtractOperator);
+    fireEvent.click(numberSeven);
+    fireEvent.click(equals);
+
+    expect(screen.getByDisplayValue('-2')).toBeInTheDocument();
+  });
+
+  it('calculates the division', () => {
+    render(<Calculator />);
+
+    const numberThree = screen.getByText('3');
+    const numberFive = screen.getByText('5');
+    const divideOperator = screen.getByText('/');
+    const numberSeven = screen.getByText('7');
+    const equals = screen.getByText('=');
+
+    fireEvent.click(numberThree);
+    fireEvent.click(numberFive);
+    fireEvent.click(divideOperator);
+    fireEvent.click(numberSeven);
+    fireEvent.click(equals);
+
+    expect(screen.getByDisplayValue('5')).toBeInTheDocument();
   });
 });
